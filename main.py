4c36d1bd23eb91d850f64d4cd68567ed346134b0
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-07-17 13:45:27 +0000

    Using RegEx (Regular Expressions) with Pandas.
    Working with and without RegEx (Regular Expressions).

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pandas as pd
-#Textbook Website: http://python-textbook.pythonhumanities.com/02_pandas/02_02_02_finding_data.html#how-to-find-specific-information-in-the-dataset-with-df-loc
+from pandas._libs.algos import diff_2d
+from pandas.core.interchange.dataframe_protocol import Column
 
 df = pd.read_csv("titanic.csv")
 
-#How to Find Column Data
-df.columns
-cols = df.columns.tolist()
+#How to Sort Data by Multiple Columns
+print(df.sort_values(["Pclass", "Sex"]).head(100))
 
-#How to Find Specific Information in the Dataset with df.loc
-#In other words, we are seeking to find those who did not survive.
-(df.loc[(df["Sex"] == "female") & (df["Pclass"] == 1) & (df["Survived"] == 0)])
+#How to Drop a Column in Pandas DataFrame
+e = df.drop(columns=["Parch", "Ticket"])
+print(e)
 
-#Total number of passengers that are identified as female, but not in first class, and then find the number of those who did not survive
+#How to Remove Rows that have NaN in any Column
+df.dropna()
 
-(df.loc[(df["Sex"] == "female") & (df["Pclass"] > 1) & (df["Survived"] == 0)])
+print(df[["Survived", "Name"]])
 
-#How to Query with “OR” (|) on a DataFrame
-print(df.loc[(df["Pclass"] == 1) | (df["Pclass"] == 2)])
+#How to Convert DataFrame Data Types (from Float to Int)
+df2.Age = df2.Age.astype(int)
+df2
