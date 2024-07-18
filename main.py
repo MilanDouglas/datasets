56eeb5fc28e8b08f4aa22883c58a987c21b2d944
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-07-18 13:28:12 +0000

    Grouping with Groupby().
    How to group data in Pandas using groupby() and some of the more powerful ways to use groupby() to manipulate quantitative data.

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
 import pandas as pd
-import re
-
 df = pd.read_csv("titanic.csv")
-df = df[["Name"]]
 
-#Finding Features within a String
-#How many reverends were there on the Titanic?
-names = df.Name.tolist()
-revs = []
-for name in names:
-  if "Rev." in name:
-    revs.append(name)
-#print(revs)
 
-#To find other data associated with "Rev."
-df1 = df.loc[df["Name"].str.contains("Rev\.")]
-print(df1)
+#Let’s try and see a DataFrame that only has “male” in the Sex column. We can do that by using get_group(“male”).
+#df = df.groupby("Sex").get_group("male")
 
-#Finding Strings that Don’t Contain Feature "Rev.".
-df2 = df.loc[~df["Name"].str.contains("Rev\.")]
-print(df2)
+#Let’s say, we want to just get all the people who are aged 20. We can do the same thing by grouping the dataset by “Age” and then getting the group of 20 year olds.
+#df = df.groupby("Age").get_group(20)
 
-#Using RegEx (Regular Expressions) with Pandas.
-#If we were interested in finding any instance of “Rev.” or “Mr.”, we would have to write something like this WITHOUT-RegEx.
-df3 = df.loc[(df["Name"].str.contains("Rev\.")) | (df["Name"].str.contains("Mr\."))]
+#Quantitative Analysis with Count() and Sum().
+#df = df.groupby("Sex").count()
+#df = df.groupby("Sex").Fare.sum()
 
-#WITH - RegEx.
-df4 = df.loc[df["Name"].str.contains("Rev\.|Mr\.", case=False,  regex=True)]
-print(df4)
+#Working with Multiple Groups.
+#Let’s say though that we are interested in how these sums divide over Pclass.
+#df = df.groupby(["Sex", "Pclass"]).Fare.sum()
 
-#To ensure that we grab both upper and lowercase forms of this sequence, let’s ignore the case by using the case keyword and setting it to False.(import re)
-df5 = df.loc[df["Name"].str.contains("Rev.\|Mr\.", case=False, regex=True)]
-print(df5)
+#Groupings with Many Subsets.
+df = pd.DataFrame(df.groupby(["Sex", "Age", "Pclass"]).Fare.sum())
+print(df)
