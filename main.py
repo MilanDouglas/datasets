5600c0922016034d840357f526f9b3bac8bba40a
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-07-18 13:54:12 +0000

    Plotting Data with Pandas
    Bar and Barh Charts with Pandas.

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import pandas as pd
 df = pd.read_csv("titanic.csv")
 
+#If we wanted to know how many male passengers were on the Titanic relative to female passengers. I could grab all the value counts and look at the numbers by calling .value_counts().
+#df = df['Sex'].value_counts()
 
-#Let’s try and see a DataFrame that only has “male” in the Sex column. We can do that by using get_group(“male”).
-#df = df.groupby("Sex").get_group("male")
-
-#Let’s say, we want to just get all the people who are aged 20. We can do the same thing by grouping the dataset by “Age” and then getting the group of 20 year olds.
-#df = df.groupby("Age").get_group(20)
-
-#Quantitative Analysis with Count() and Sum().
-#df = df.groupby("Sex").count()
-#df = df.groupby("Sex").Fare.sum()
-
-#Working with Multiple Groups.
-#Let’s say though that we are interested in how these sums divide over Pclass.
-#df = df.groupby(["Sex", "Pclass"]).Fare.sum()
-
-#Groupings with Many Subsets.
-df = pd.DataFrame(df.groupby(["Sex", "Age", "Pclass"]).Fare.sum())
+#We can take that initial code we see above and append two other methods to it .plot.bar().
+df = df['Sex'].value_counts().plot.bar()
 print(df)
+
--- replit.nix
+++ replit.nix
@@ -2,6 +2,16 @@
 #pkgs.python38
 {pkgs}: {
   deps = [
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
     pkgs.glibcLocales
     pkgs.sqlite
     pkgs.unzip
